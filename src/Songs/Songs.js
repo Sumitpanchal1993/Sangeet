48e4c9412554b1767ab50b59fa7e6e568a4167c7
Sumitpanchal1993 <sumitpanchal08@gmail.com>
2024-11-21 23:15:32 +0530

    First commit

---
+++ src/Songs/Songs.js
@@ -0,0 +1,10 @@
+import song1 from './128-Aayi Nai - Stree 2 128 Kbps.mp3'
+import song2 from './128-Dil Kehta Hai - Akele Hum Akele Tum 128 Kbps.mp3'
+import song3 from './Aaj Ki Raat(PagalWorld.com.sb).mp3'
+import song4 from './Jatt Mehkma(PagalWorld.com.sb).mp3'
+import song5 from './Millionaire(PagalWorld.com.sb).mp3'
+
+const songsList = [song1, song2, song3, song4, song5]
+
+
+export default songsList
